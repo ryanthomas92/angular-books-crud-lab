979cddaa96c919f47a1214a71c7d32aa025cede8
ryanthomas92 <rthoma11@villanova.edu>
2017-01-06 15:47:21 -0800

    should have committed more, but all data from books loads

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -3,3 +3,21 @@ angular.module('libraryApp')
 
 // add your BooksIndexController function here!
 // don't forget $http if you need to make requests
+
+BooksIndexController.$inject=['$http'];
+
+function BooksIndexController ($http) {
+  console.log("coolio ");
+  var vm = this;
+  var newBook = {};
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(response) {
+    vm.books = response.data.books;
+    console.log(vm.books);
+  }, function errorCallback(error) {
+    console.log("There was an error " + error);
+  });
+}
