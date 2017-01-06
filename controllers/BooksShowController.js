9ab1ab90ea54168340b5a6f85dd369b26170ef59
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2017-01-06 10:30:42 -0800

    Update BooksShowController.js

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,10 @@
 angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
-BooksShowController.$inject=['$http', '$routeParams', '$location'];
-function BooksShowController($http, $routeParams, $location) {
+// controller function and dependency injection
+// $routeParams and $location are required for routing stuff 
+//   - but you might need to add a dependency
+BooksShowController.$inject=['$routeParams', '$location'];   
+function BooksShowController($routeParams, $location) {
   var vm = this;
 };
