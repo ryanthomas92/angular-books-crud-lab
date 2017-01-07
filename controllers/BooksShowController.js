ae01e124d913dfac6a7f60603958a7188572e3a2
ryanthomas92 <rthoma11@villanova.edu>
2017-01-06 17:26:23 -0800

    got delete to work and switch pages on title click

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -2,9 +2,50 @@ angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
 // controller function and dependency injection
-// $routeParams and $location are required for routing stuff 
+// $routeParams and $location are required for routing stuff
 //   - but you might need to add a dependency
-BooksShowController.$inject=['$routeParams', '$location'];   
-function BooksShowController($routeParams, $location) {
+BooksShowController.$inject=['$routeParams', '$location', '$http'];
+function BooksShowController($routeParams, $location, $http) {
   var vm = this;
+  var bookId = $routeParams.id;
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books/' + bookId
+  }).then(function successCallback(response) {
+    vm.book = response.data;
+    console.log(vm.book);
+  }, function errorCallback(error) {
+    console.log("There was an error " + error);
+  });
+
+  vm.editBook = function(book) {
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + bookId,
+      data: {
+        title: book.title,
+        author: book.author,
+        releaseDate: book.releaseDate,
+        image: book.image
+      },
+    }).then(function successCallback(editedBook) {
+      // var index = vm.book.indexOf(editedBook);
+      // vm.book.splice(index, 1, editedBook);
+      // $location.path('/');
+    }, function errorCallback(error) {
+      console.log("There's an error " + error);
+    });
+  }
+
+  vm.deleteBook = function(book) {
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + bookId
+    }).then(function successCallback(deletedBook) {
+      $location.path('/');
+    }, function errorCallback(error) {
+      console.log("There's an error ", error);
+    });
+  }
 };
